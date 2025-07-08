184eaa5f45cc7d9bce40d8c68243cab08f3bed83
Isaac Yawson <isaacayawson1996@gmail.com>
2025-07-07 21:45:43 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+!/usr/bin/env python3
+""" Main file to test Cache and replay function """
+
+from exercise import Cache, replay
+
+cache = Cache()
+cache.store("foo")
+cache.store("bar")
+cache.store(42)
+
+replay(cache.store)
